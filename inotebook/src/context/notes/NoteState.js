cc325bb9abaf55c365b67cee8c49bda17e3abe94
ShivanshBharatBhushanSharma <shivansh.sharmas1999@gmail.com>
2023-05-26 16:10:00 +0530

    Adding AddNote Component to iNotebook

--- inotebook/src/context/notes/NoteState.js
+++ inotebook/src/context/notes/NoteState.js
@@ -24,8 +24,34 @@ const NoteState = (props) => {
   ];
 
   const [notes, setNotes] = useState(notesInitial);
+  
+  // Add a Note
+  const addNote = (title, description, tag) =>{
+    // TODO: API call
+    const note = {
+      _id: "646874313449e408f448f325",
+      user: "6465bc85ee9e60cb368ad3de",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2023-05-20T07:18:09.181Z",
+      __v: 0,
+    };
+    setNotes(notes.concat(note))
+  }
+
+  // Delete a Note
+  const deleteNote = () =>{
+
+  }
+
+  // Edit a Note
+  const editNote = () =>{
+
+  }
+
   return (
-    <NoteContext.Provider value={{ notes, setNotes }}>
+    <NoteContext.Provider value={{ notes, setNotes, addNote, deleteNote, editNote }}>
       {props.children}
     </NoteContext.Provider>
   );
