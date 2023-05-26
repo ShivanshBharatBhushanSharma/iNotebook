97ca33db02329136ae74b80b560b62b107c3fc66
ShivanshBharatBhushanSharma <shivansh.sharmas1999@gmail.com>
2023-05-26 12:41:02 +0530

    Fetching Notes from Context

--- inotebook/src/context/notes/NoteState.js
+++ inotebook/src/context/notes/NoteState.js
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    
+  const notesInitial = [
+    {
+      _id: "646857f5eeff9ccefe33084b",
+      user: "6465bc85ee9e60cb368ad3de",
+      title: "My Title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2023-05-20T05:17:41.161Z",
+      __v: 0,
+    },
+    {
+      _id: "646874313449e408f448f325",
+      user: "6465bc85ee9e60cb368ad3de",
+      title: "New Note",
+      description: "React JS Course Completed",
+      tag: "youtube",
+      date: "2023-05-20T07:18:09.181Z",
+      __v: 0,
+    },
+  ];
+
+  const [notes, setNotes] = useState(notesInitial);
   return (
-    <NoteContext.Provider value={{}}>
-        {props.children}
+    <NoteContext.Provider value={{ notes, setNotes }}>
+      {props.children}
     </NoteContext.Provider>
-  )
+  );
 };
 
 export default NoteState;
